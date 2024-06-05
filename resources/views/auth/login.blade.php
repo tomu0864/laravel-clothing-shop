3280077f71157ed62285b04d69cd63b0f0fdfb6b
Tomu <fantasista2150@outlool.jp>
2024-06-06 02:28:14 +0900

    add title to register and login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', 'login')
 
 @section('content')
 <div class="container">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', 'register')
 
 @section('content')
 <div class="container">
